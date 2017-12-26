7330d2ca942132b5231cdc536f63f71911269afc
dimitris <dlolis88@gmail.com>
2017-12-26 19:14:59 +0200

    first feature

--- feature_selector.py
+++ feature_selector.py
@@ -1,11 +1,16 @@
 import pandas as pd
 
 
-def join(how, left, right, collumns, keys, name=None):
+def join(left, right, params):
+
+    how = params.get('how')
+    name = params.get('name')
+    keys = params.get('keys')
+    columns = params.get('columns')
     new = left.merge(right, how=how, on=keys)
     if new.isnull().values.any():
         new.fillna(0)
-    new.drop(collumns, inplace = True, axis =1)
+    new.drop(columns, inplace = True, axis =1)
     if name:
         new.to_csv(name,index = False)
     return new
--- main.py
+++ main.py
@@ -4,19 +4,29 @@ from prediction import get_prediction # when we are ready to test it.
 import pandas as pd
 
 X_train, X_test, y_train, y_test = phase("testing")
-
 order_products_priod_df = pd.read_csv("data/order_products__prior.csv")
 products_df = pd.read_csv("data/products.csv")
-how = 'left'
-keys = 'product_id'
 
-columns = ['product_name', 'aisle_id', 'add_to_cart_order','department_id','product_id']
-feature = join(how, order_products_priod_df, products_df, columns, keys)
+params = {
+    'how': 'left',
+    'key': 'product_id',
+    'columns': ['aisle_id', 'add_to_cart_order','product_id','product_name'],
+    'name': None
+}
 
-print feature.head(5)
 
-X_train = X_train.merge(feature, how=how, on='order_id')
+feature = join(order_products_priod_df, products_df, params)
 
-if X_train.isnull().values.any():
-    X_train.fillna(0)
-get_prediction(X_train, X_test, y_train, y_test)
+
+fun = lambda x: 1 if x == 7 else 0
+feature["department_id"]= feature['department_id'].apply(fun) # i turn every non department 7 id to 0 and every 7 to 1
+
+feature = feature[feature["department_id"]==1]
+
+
+X_train = X_train.merge(feature, how='left', on='order_id')
+X_train.fillna(0, inplace=True)
+
+X_test = X_test.merge(feature, how='left', on='order_id')
+X_test.fillna(0, inplace = True)
+get_prediction(X_train, X_test,y_train,y_test)
