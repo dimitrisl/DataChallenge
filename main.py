53627f339135c4a56cd0f635e77bca56f1a7bd3f
dimitris <dlolis88@gmail.com>
2017-12-28 04:08:42 +0200

    0.51 accuracy

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ import numpy as np
 X_train, X_test, y_train, y_test = phase("testing")
 order_products_priod_df = pd.read_csv("data/order_products__prior.csv")
 products_df = pd.read_csv("data/products.csv")
+orders_df = pd.read_csv("data/orders.csv")
+products_df=pd.read_csv("data/products.csv")
 
 params = {
     'how': 'left',
@@ -17,34 +19,35 @@ params = {
 
 feature = join(order_products_priod_df, products_df, params)
 fun = lambda x: 1 if x == 7 else 0
-
 feature["department_id"] = feature['department_id'].apply(fun) # i turn every non department 7 id to 0 and every 7 to 1
 
 pf = feature.copy()
 
 feature = feature.groupby("order_id", as_index = False).department_id.sum()
-print feature.head(5)
-
 pf = pf.groupby("order_id", as_index = False).add_to_cart_order.max()
-print pf.head(5)
-
 rat = feature.merge(pf, how="left", on="order_id")
-
 rat = rat.rename(columns={"department_id":"beverages"})
-print rat.head(15)
+column = rat["beverages"]/rat["add_to_cart_order"]
+column.columns = ["ratio"]
+column = column.replace(0,np.nan)
+column=column.dropna(how='all',axis=0)
+column=column.replace(np.nan,0)
+# drop the ratio of 0
 
-# x = raw_input(" ")
-#
-# print feature.head(6)
-# X_train = X_train.merge(feature, how='left', on='order_id')
-# X_train.fillna(0, inplace=True)
-# print X_train.head(6)
-#
-# X_test = X_test.merge(feature, how='left', on='order_id')
-# X_test.fillna(0, inplace = True)
-#
-# get_prediction(X_train, X_test, y_train, y_test)
-# #clear the memory
-# del products_df
-# ###
-#
+new_feature = pd.concat([rat["order_id"], column],axis=1,keys=['rat','column'])
+new_feature.columns = ["order_id","ratio"]
+feature1 = orders_df.merge(new_feature, how="left", on="order_id")
+feature1.drop(['user_id',"order_number","order_dow","order_hour_of_day","days_since_prior_order"], inplace = True, axis =1)
+feature1.fillna(0,inplace=True)
+su = orders_df.merge(feature1,how="left",on="order_id")
+su.drop(['order_number', 'order_dow','order_hour_of_day','days_since_prior_order'], inplace = True, axis =1)
+X_train = X_train.merge(su, how='left', on=['order_id','user_id'])
+X_train.fillna(0, inplace=True)
+print X_train.head(10)
+
+X_test = X_test.merge(su, how='left', on=['order_id','user_id'])
+X_test.fillna(0, inplace = True)
+
+get_prediction(X_train, X_test, y_train, y_test)
+
+
--- prediction.py
+++ prediction.py
@@ -5,6 +5,9 @@ from sklearn.linear_model import LogisticRegression
 def get_prediction(X_train, X_test, y_train, y_test):
 
     logreg = LogisticRegression()
+    X_train.drop(['user_id','order_id'], axis=1, inplace=True)
+    X_test.drop(['user_id','order_id'], axis=1, inplace=True)
+    y_train.drop('order_id', axis=1, inplace=True)
     logreg.fit(X_train, y_train["category"])
     y_pred = logreg.predict(X_test)
     print "the logistic regression result is", y_pred
