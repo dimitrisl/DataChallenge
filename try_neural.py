b2f2cb0f2e5915ffea9149d775e53e5fc8210d62
dimitris <dlolis88@gmail.com>
2018-01-08 01:12:22 +0200

    last try

--- try_neural.py
+++ try_neural.py
@@ -4,23 +4,21 @@ from sklearn.model_selection import train_test_split
 
 import numpy
 import pandas as pd
-from initial_data import phase
 numpy.random.seed(7)
 
-X_train = pd.read_csv("feature_selection_x_train.csv")
+X_train = pd.read_csv("x_train.csv")
 y_train = pd.read_csv("data/y_train.csv")
-X_test = pd.read_csv("feature_selection_x_test.csv")
+X_test = pd.read_csv("x_test.csv")
 y_train.drop('order_id', axis=1, inplace=True)
 # # #
 X_train, X_test, y_train, y_test = train_test_split(X_train, y_train,test_size=0.33)
 
 model = Sequential()
-model.add(Dense(12, input_dim=11, activation='relu'))
-#model.add(Dense(10, activation='tanh'))
+model.add(Dense(24, input_dim=14, activation='tanh'))
+#model.add(Dense(32, activation='tanh'))
 model.add(Dense(1, activation='sigmoid'))
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-model.fit(X_train, y_train, epochs=10000, batch_size=X_train.shape[0])
-
+model.fit(X_train, y_train, epochs=5000, batch_size=X_train.shape[0])
 
 #evaluate the model
 scores = model.evaluate(X_test, y_test)
